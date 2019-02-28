4b14ebf220be5e116107b41018b62fcfaf887781
Abhay <45193704+Teliat@users.noreply.github.com>
2019-02-28 14:38:22 +0530

    My message

--- Jankins.bat
+++
@@ -1,4 +0,0 @@
-set projectLocation=D:\Telia\Testing\Selenium
-cd /d %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
